90f2329447e67e0783f900cdd50f9ef82ca7c196
be-goood <begooood152@gmail.com>
2020-05-21 10:06:51 -0600

    Unit test

--- Employee/Employee.Logic.Tests/EmployeeTransactionsTests.cs
+++ Employee/Employee.Logic.Tests/EmployeeTransactionsTests.cs
@@ -16,7 +16,6 @@ namespace Employee.Logic.Tests
         public async Task GetAllEmployeesWithCurrentSalariesAsync_MoreThan1Salary_ReturnCurrentSalary()
         {
             // Arrange
-
             var salaries = new List<EmployeeSalary>()
             {
                 new EmployeeSalary() { StartDate = new DateTime(2019,1,1), EndDate = new DateTime(2019,12,1), YearlyWages = 125000 },
