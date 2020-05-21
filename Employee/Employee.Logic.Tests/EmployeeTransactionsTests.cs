d7a0c1134e21467cc324863651c55eb214413d79
be-goood <begooood152@gmail.com>
2020-05-21 10:04:13 -0600

    Employee unit test

--- Employee/Employee.Logic.Tests/EmployeeTransactionsTests.cs
+++ Employee/Employee.Logic.Tests/EmployeeTransactionsTests.cs
@@ -12,27 +12,29 @@ namespace Employee.Logic.Tests
     [TestClass]
     public class EmployeeTransactionsTests
     {
-        //[TestMethod]
-        //public async Task GetEmployeeWithoutCurrentSalaryAsync_2Salaries_ReturnActiveSalary()
-        //{
-        //    // arrange
-        //    var salaries = new List<EmployeeSalary>()
-        //    {
-        //        new EmployeeSalary() { StartDate = new DateTime(2019,1,1), EndDate = new DateTime(2019,12,1), YearlyWages = 125000 },
-        //        new EmployeeSalary() { StartDate = new DateTime(2019,1,1), EndDate = null, YearlyWages = 150000 },
-        //    };
-        //    var employee = new EmployeeEntity() { FirstName = "Jorge", LastName = "Velazquez", Salary = salaries };
+        [TestMethod]
+        public async Task GetAllEmployeesWithCurrentSalariesAsync_MoreThan1Salary_ReturnCurrentSalary()
+        {
+            // arrange
 
+            var salaries = new List<EmployeeSalary>()
+            {
+                new EmployeeSalary() { StartDate = new DateTime(2019,1,1), EndDate = new DateTime(2019,12,1), YearlyWages = 125000 },
+                new EmployeeSalary() { StartDate = new DateTime(2019,1,1), EndDate = null, YearlyWages = 52000 },
+            };
+            var employee = new EmployeeEntity() { FirstName = "Jorge", LastName = "Velazquez", Salary = salaries };
+            var employees = new List<EmployeeEntity>() { employee };
 
-        //    var employeeId = Guid.Parse("1608CA7F-4BD6-4F8D-8C20-93F8637F1052");
-        //    var mock = new Mock<IQuery<EmployeeEntity, Guid>>();
-        //    mock.Setup(p => p.ExecuteQueryAsync(employeeId)).Returns(Task.FromResult(employee));
+            var employeeId = Guid.Parse("1608CA7F-4BD6-4F8D-8C20-93F8637F1052");
+            var mock = new Mock<IQueryNoParam<List<EmployeeEntity>>>();
+            mock.Setup(p => p.ExecuteQueryAsync()).Returns(Task.FromResult(employees));
 
-        //    // assert
-        //    var result = await new EmployeeTransactions().GetEmployeeWithoutSalaryAsync(mock.Object, employeeId);
+            // assert
+            var result = await new EmployeeTransactions().GetAllEmployeesWithCurrentSalariesAsync(mock.Object);
 
-        //    // act
-        //    Assert.AreEqual(150000, result.Salary.First().YearlyWages);
-        //}
+            // act
+            Assert.IsNull(result.First().Salary.First().EndDate);
+            Assert.AreEqual(52000, result.First().Salary.First().YearlyWages);
+        }
     }
 }
